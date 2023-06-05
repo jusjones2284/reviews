2dd54af3c1abd0886b195df7987bb0a1b87edeb2
Justin Jones <jus.jones@yahoo.com>
2023-06-05 14:20:24 -0400

    added the react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import people from './data';
+import {FaChevronCircleLeft, FaChevronCircleRight, FaQuoteRight} from 'react-icons/fa'
 
 const App = () => {
   const [index, setIndex] = useState(0)
   const {name, job, image, text} = people[index]
-  console.log(name);
-  return <h2>Reviews Starter</h2>;
+  // console.log(name);
+  return <div>
+          <h2>Reviews Starters</h2>
+          
+         </div>
+  
+ 
 };
 export default App;
